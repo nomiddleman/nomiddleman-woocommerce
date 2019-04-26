03b02112f843d10dd1f5fe03de63af3dc7ea54b0
rgostic <rgostic@outlook.com>
2019-04-25 23:57:40 -0400

    Added some animation to sample address generation.

--- assets/js/nmm.js
+++ assets/js/nmm.js
@@ -67,20 +67,38 @@ jQuery(document).ready(function() {
 					},
 				beforeSend: function () {
 					updateSampleText(cryptoId, numberOfSamples, 'Generating HD Addresses');
+					
+						//jQuery('#nmmpro_redux_options-BTC_hd_mpk_sample_addresses').parent().prev().append('<div id="floatingCirclesG">');
+						//jQuery('#nmmpro_redux_options-BTC_hd_mpk_sample_addresses').fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+						jQuery('#nmmpro_redux_options-BTC_hd_mpk_sample_addresses input').addClass('flash');
+					for (let i = 1; i < 9; i++) {
+						jQuery('#floatingCirclesG')
+							.append('<div class="f_circleG" id="frotateG_0' + i + '"></div>');
+						
+							
+	
+		
+					}
+				 
 				}
 			}).fail(function() {
-				updateSampleText(cryptoId, numberOfSamples, 'HD Address generation failed, please double check your mpk');				
+				updateSampleText(cryptoId, numberOfSamples, 'Address gen failed, please check your mpk');				
+				jQuery('#nmmpro_redux_options-BTC_hd_mpk_sample_addresses input')
+						.css('background-color', '#eee')
+						.css('color', 'black');
+				jQuery('#floatingCirclesG').hide();
 				console.log(stuff);
 			}).done(function(ajaxResponse) {
 				console.log('success');
-				
+				jQuery('#floatingCirclesG').hide();
+				jQuery('#nmmpro_redux_options-BTC_hd_mpk_sample_addresses input').removeClass('flash');
 				addresses = JSON.parse(ajaxResponse);
 
 				if (addresses[0] === 'You have entered a valid Segwit MPK.') {
 					updateSampleText(cryptoId, numberOfSamples, '');
 					jQuery('#' + cryptoId + '_hd_mpk_sample_addresses-0').val(addresses[0]);
 					if (jQuery('#' + cryptoId + '_hd_mpk_sample_addresses-0').parent().children().length === 2) {
-						jQuery('#' + cryptoId + '_hd_mpk_sample_addresses-0').parent().append(addresses[1]);		
+						jQuery('#' + cryptoId + '_hd_mpk_sample_addresses-0').parent().append(addresses[1]);
 					}
 					
 				}
